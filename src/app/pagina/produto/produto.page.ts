8633fdf1978a3b88933c3d6705c21b9c18f138f3
AwaitSistemas <abmael.ninha@gmail.com>
2021-07-31 22:20:07 -0400

    153 - Componente loading

--- src/app/pagina/produto/produto.page.ts
+++ src/app/pagina/produto/produto.page.ts
@@ -3,6 +3,7 @@ import { ProdutoService } from './../../../service/domain/produto.service';
 import { ProdutoDTO } from './../../../models/produto.dto';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-produto',
@@ -12,46 +13,71 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProdutoPage implements OnInit {
 
   items: ProdutoDTO[];
+  loading: any;
 
   constructor(
 
     public activatedRouter: ActivatedRoute,
     public produtoService: ProdutoService,
-    private router: Router
-  ) { 
+    private router: Router,
+    public loadingController: LoadingController
+
+  ) {
 
   }
 
   ngOnInit() {
     let categoria_id = this.activatedRouter.snapshot.paramMap.get('data');
-    
-    this.produtoService.findByCategoria(categoria_id)    
-    .subscribe(response => {
-      this.items = response['content'];
-      this.loadImageUrls();
-    },
-    
-    error =>{
+    this.presentLoading();
+    this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+        this.hideLoader();
+        this.loadImageUrls();
+      },
 
-      });
+        error => {
+
+        });
   }
 
   loadImageUrls() {
-    for(var i = 0; i< this.items.length; i++){
+    for (var i = 0; i < this.items.length; i++) {
       let item = this.items[i];
-    this.produtoService.getSmallImageFromBucket(item.id)
-      .subscribe(response =>{
-        item.imageUrl = `${API_CONFIG.backetBaseUrl}/prod${item.id}-small.jpg`;
-      },
-      error =>{});
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.backetBaseUrl}/prod${item.id}-small.jpg`;
+        },
+          error => { });
     }
 
   }
 
-  showDetail(produto_id: string){
-     let data = JSON.stringify(produto_id);
-    this.router.navigate(['produto-detail', {data}]);
+  showDetail(produto_id: string) {
+    let data = JSON.stringify(produto_id);
+    this.router.navigate(['produto-detail', { data }]);
   }
+
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Aguarde...',
+      spinner: 'lines'
+    }).then((res) => {
+      res.present();
+    })
+  }
+  hideLoader() {
+
+    this.loadingController.dismiss().then((res) => {
+      console.log('Loading dismissed!', res);
+    }).catch((error) => {
+      console.log('error', error);
+    });
+
+  }
+
+
 }
 
 
